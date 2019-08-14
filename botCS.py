cc61b109bf3929f36d7d7e2e452214eac983bc0b
stellandyt <49580785+stellandyt@users.noreply.github.com>
2019-08-14 13:37:04 +0300

    Update botCS.py

--- botCS.py
+++ botCS.py
@@ -1,103 +1,119 @@
 import telebot
-import pyowm
-# import logging
-# from flask import Flask, request
-# import os
 from telebot import types
+import requests
+from telebot import apihelper
+import time
 
-token = "767640365:AAGLF3nApdXdsJUYXMC-V5VDRIiMaLYoGN8"
-tokenOWM = "c687e4132128127329716ffed7313e70"
 
+token = '918573896:AAEM0r2hDXAJoCwy5WtTFZJ92iEmhxmoVeM'
 bot = telebot.TeleBot(token)
-owm = pyowm.OWM(tokenOWM, language='ru')
 
-aaa = False
-city = ""
+apihelper.proxy = {"https": "socks5://163.172.81.30:443"}
+
 
 @bot.message_handler(commands=['start'])
-def Start_bot(message):
-    bot.send_message(
-        message.chat.id,
-        'Привет! :)\nИспользуй: /help,\nчтобы узнать список доступных команд!.\n'
-    )
+def start(message):
+    bot.send_message(message.chat.id, 'Привет! Ты зашёл в нашу игру!', reply_markup=keyboard())
 
-@bot.message_handler(commands=['help'])
-def help_command(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(
-        telebot.types.InlineKeyboardButton(
-            'Создатель', url='telegram.me/Stelland'
-  )
-    )
-    bot.send_message(
-        message.chat.id,
-        '1) Хочешь узнать погоду в своём городе? Напиши просто название города, и узнай температуру! Всё просто :).\n' +
-        '2) Раз, два, три.\n' +
-        '3) Ещё текст',
-        reply_markup=keyboard
-    )
 
-@bot.message_handler(commands=['vk'])
-def vk_sozdatelya(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(
-        telebot.types.InlineKeyboardButton(
-            'Жмякай', url='vk.com/stelland'
-        )
-    )
-    bot.send_message(
-        message.chat.id,
-        'Создатель есть и Вконтакте :)',
-        reply_markup=keyboard
-    )
+def get_id(message):
+    text = message.text
+    serv = 'http://api.warface.ru/user/stat/?name=%s&server=1' % text
+    print(serv)
+    r = requests.get(serv)
+    t = str(r)
+    data = r.json()
+    if t == '<Response [400]>':
+        serv = 'http://api.warface.ru/user/stat/?name=%s&server=2' % text
+        print(serv)
+        r = requests.get(serv)
+        t = str(r)
+        if t == '<Response [400]>':
+            serv = 'http://api.warface.ru/user/stat/?name=%s&server=3' % text
+            print(serv)
+            r = requests.get(serv)
+            t = str(r)
+            if t == '<Response [200]>':
+                data = r.json()
+                print(data)
+                nick = data['nickname']
+                rank = data['rank_id']
+                exp = data['experience']
+                try:
+                    clan = data['clan_name']
+                    bot.send_message(message.chat.id,
+                                     'Ник: ' + str(nick) + '\n' + 'Сервер: Чарли' + '\n' + 'Ранг: ' + str(
+                                         rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: ' + str(clan),
+                                     reply_markup=keyboard())
+                except KeyError:
+                    bot.send_message(message.chat.id,
+                                     'Ник: ' + str(nick) + '\n' + 'Сервер: Чарли' + '\n' + 'Ранг: ' + str(
+                                         rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: No',
+                                     reply_markup=keyboard())
+            else:
+                bot.send_message(message.chat.id, 'Игрок скрыл свою статистику или данный Ник не найден.', reply_markup=keyboard())
+                print('error')
 
-@bot.message_handler(commands=["geophone"])
-def geophone(message):
-    # Эти параметры для клавиатуры необязательны, просто для удобства
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Узнать свой номер телефона", request_contact=True)
-    button_geo = types.KeyboardButton(text="Узнать своё местоположение", request_location=True)
-    btn_back = types.KeyboardButton(text='Назад')
+        elif t == '<Response [200]>':
+            data = r.json()
+            print(data)
+            nick = data['nickname']
+            rank = data['rank_id']
+            exp = data['experience']
+            try:
+                clan = data['clan_name']
+                bot.send_message(message.chat.id,
+                                 'Ник: ' + str(nick) + '\n' + 'Сервер: Браво' + '\n' + 'Ранг: ' + str(
+                                     rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: ' + str(clan),
+                                 reply_markup=keyboard())
+            except KeyError:
+                bot.send_message(message.chat.id,
+                                 'Ник: ' + str(nick) + '\n' + 'Сервер: Браво' + '\n' + 'Ранг: ' + str(
+                                     rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: No',
+                                 reply_markup=keyboard())
 
-    keyboard.add(button_phone, button_geo)
-    keyboard.add(btn_back)
+    elif t == '<Response [200]>':
+        data = r.json()
+        print(data)
+        nick = data['nickname']
+        rank = data['rank_id']
+        exp = data['experience']
+        try:
+            clan = data['clan_name']
+            bot.send_message(message.chat.id,
+                            'Ник: ' + str(nick) + '\n' + 'Сервер: Альфа' + '\n' + 'Ранг: ' + str(
+                                rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: ' + str(clan),
+                            reply_markup=keyboard())
+        except KeyError:
+            bot.send_message(message.chat.id,
+                             'Ник: ' + str(nick) + '\n' + 'Сервер: Альфа' + '\n' + 'Ранг: ' + str(
+                                 rank) + '\n' + 'Exp: ' + str(exp) + '\n' + 'Клан: No',
+                             reply_markup=keyboard())
 
-    bot.send_message(message.chat.id, "Отправь мне свой номер телефона или поделись местоположением, жалкий человечишка!", reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    # global aaa, city
-    # bot.send_message(message.chat.id, "Введите город/страну!")
-    city = message.text
-    aaa = True
-
-    if aaa == True:
-        try:
-            observation = owm.weather_at_place(city)
-            w = observation.get_weather()
-            temp = w.get_temperature('celsius')["temp"]
-
-            answer = "В г." + city + ',' + " сейчас: " + '\n--> ' + w.get_detailed_status() + ' <--' + "\n"
-            answer += "Температура сейчас, в районе: " + str(temp) + " °C"
-
-            bot.send_message(message.from_user.id, answer)
-            print(answer)
-            asd = False
-
-        except pyowm.exceptions.api_response_error.NotFoundError:
-            bot.send_message(message.from_user.id, "Я тебя не понимаю :(", reply_markup=keyboard())
-
+def get_text_message(message):
+    global gg, check
+    text = message.text
+    if text == 'Привет' or text == 'привет':
+        bot.send_message(message.chat.id, 'Хааааай', reply_markup=keyboard())
+    elif text == 'Данные игрока':
+        sent = bot.send_message(message.chat.id, 'Введите ник игрока.')
+        bot.register_next_step_handler(sent, get_id)
+    else:
+        bot.send_message(message.chat.id, 'Я тебя не понимаю!', reply_markup=keyboard())
 
 
 def keyboard():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    btn1 = types.KeyboardButton('/help')
-    btn2 = types.KeyboardButton('/vk')
-    btn3 = types.KeyboardButton('/geophone')
-
-    markup.add(btn1, btn2)
-    markup.add(btn3)
+    btn_1 = types.KeyboardButton('Help')
+    btn_8 = types.KeyboardButton('Данные игрока')
+    markup.add(btn_1, btn_8)
     return markup
 
-
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(none_stop=True, timeout=123, interval=0)
+    except Exception as E:
+        print(E.args)
+        time.sleep(2)
